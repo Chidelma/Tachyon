02455b2519d98f3cf8945e8a00a9b0c607e7ed5e
chidelma <samuel@clickspace.com>
2023-12-27 13:37:10 -0500

    returned serve function

--- src/serve.ts
+++ src/serve.ts
@@ -33,52 +33,55 @@ async function transformRequest(req: Request, contentType: string) {
     return await req.blob() 
 }
 
-export function SERVE(target: Function) {
+export function SERVE() {
 
-    const server = Bun.serve({async fetch(req: Request) {
+    return function(target: Function) {
 
-        const url = new URL(req.url)
+        const server = Bun.serve({async fetch(req: Request) {
 
-        host = url.host, port = url.port
+            const url = new URL(req.url)
     
-        const [file, func] = url.pathname.split('/').slice(1, 3)
-    
-        // @ts-ignore
-        const controller = new target().constructor
-    
-        if(controller[func] === undefined) throw new Error(`${func} route does not exist in ${file} controller`, { cause: 404 })
-    
-        const route = controller[func]
-
-        const contentType = req.headers.get('content-type')
-
-        let data = undefined
-
-        const paths = new URL(req.url).pathname.split('/').slice(3)
-
-        startTime = Date.now()
-
-        if(contentType) {
+            host = url.host, port = url.port
         
-            data = await route(await transformRequest(req, contentType), req.headers)
-
-        } else if(paths.length > 0) {
-
-            data = await route(...parsePaths(paths), req.headers)
-
-        } else data = await route(req.headers)
-    
-        Logger.INFO(`http://${host}:${port} - "${req.method} ${url.pathname} ${url.protocol}" 200 OK - ${Date.now() - startTime}ms - ${typeof data !== 'undefined' ? String(data).length : 0} bytes`)
+            const [file, func] = url.pathname.split('/').slice(1, 3)
         
-        return typeof data === 'object' ? Response.json(data, { status: 200 }) : new Response(data, { status: 200 })
+            // @ts-ignore
+            const controller = new target().constructor
+        
+            if(controller[func] === undefined) throw new Error(`${func} route does not exist in ${file} controller`, { cause: 404 })
+        
+            const route = controller[func]
     
-    }, error(req) {
-
-        Logger.ERROR(`http://${host}:${port} - ${ req.cause ?? 500 } ${Date.now() - startTime}ms - ${req.message.length} bytes`)
+            const contentType = req.headers.get('content-type')
     
-        return Response.json({ detail: req.message }, { status: req.cause as number ?? 500 })
+            let data = undefined
     
-    }, port: 8000 })
-
-    Logger.INFO(`Server is running on http://${server.hostname}:${server.port} (Press CLTRL+C to quit)`)
+            const paths = new URL(req.url).pathname.split('/').slice(3)
+    
+            startTime = Date.now()
+    
+            if(contentType) {
+            
+                data = await route(await transformRequest(req, contentType), req.headers)
+    
+            } else if(paths.length > 0) {
+    
+                data = await route(...parsePaths(paths), req.headers)
+    
+            } else data = await route(req.headers)
+        
+            Logger.INFO(`http://${host}:${port} - "${req.method} ${url.pathname} ${url.protocol}" 200 OK - ${Date.now() - startTime}ms - ${typeof data !== 'undefined' ? String(data).length : 0} bytes`)
+            
+            return typeof data === 'object' ? Response.json(data, { status: 200 }) : new Response(data, { status: 200 })
+        
+        }, error(req) {
+    
+            Logger.ERROR(`http://${host}:${port} - ${ req.cause ?? 500 } ${Date.now() - startTime}ms - ${req.message.length} bytes`)
+        
+            return Response.json({ detail: req.message }, { status: req.cause as number ?? 500 })
+        
+        }, port: 8000 })
+    
+        Logger.INFO(`Server is running on http://${server.hostname}:${server.port} (Press CLTRL+C to quit)`)
+    }
 }
