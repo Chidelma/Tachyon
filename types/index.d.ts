ec1e25e7d540deda17523ee4f720701b53005214
chidelma <samuel@clickspace.com>
2024-07-15 11:02:43 -0400

    feat: import arrayview from bun

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-import { FileSink } from "bun"
+import { FileSink, ArrayBufferView } from "bun"
 
 interface _WSContext {
     request: Request
@@ -8,7 +8,7 @@ interface _WSContext {
 interface _HTTPContext {
     request: Request
     subscribe?: (topic: string) => void
-    publish: (topic: string, data: string | ArrayBuffer | SharedArrayBuffer | import("bun").ArrayBufferView, compress?: boolean) => number
+    publish: (topic: string, data: string | ArrayBuffer | SharedArrayBuffer | ArrayBufferView, compress?: boolean) => number
     requestTime?: number,
     ipAddress: string,
     logWriter?: FileSink,
